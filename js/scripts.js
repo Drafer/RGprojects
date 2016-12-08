800b78b6a73d926df66ccad2fff113891a0d4de1
Rafal <rafalgrzenia96@gmail.com>
2016-12-08 12:41:47 +0100

    js file

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt("Podaj a");
+var h = prompt("Podaj h");
+var triangleArea = a * h /2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
