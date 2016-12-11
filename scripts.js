d656d570112f415be39ed157e2cfdd3e1890d73d
Rafal <rg@gmail.com>
2016-12-11 12:13:08 +0100

    nowy js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var a = prompt("podaj a");
 var b = prompt("podaj b");
 var value = (a*a) + (2 * a * b) - (b*b);
-document.write("Wynik to: " + value + " jest");
+document.write("Wynik to: " + value + "  jest");
 
 if (value < 0) {
 	document.write(" ujemny");
