c15806e7469f825680fa316c0697295ff3f0bbc1
Rafal <rg@gmail.com>
2016-12-11 12:08:08 +0100

    js file

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var a = prompt("podaj a");
+var b = prompt("podaj b");
+var value = (a*a) + (2 * a * b) - (b*b);
+document.write("Wynik to: " + value + " jest");
+
+if (value < 0) {
+	document.write(" ujemny");
+
+} else if (value > 0) {
+	document.write(" dodatni");
+}
+
+if (value == 0) {
+	document.write(" zerowy")
+}
